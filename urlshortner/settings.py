6e69201a70bbc201f6fa06bbe3a0c4ca62e09391
PeisenWong <wongpeisen@gmail.com>
2021-07-15 20:14:42 +0800

    I will be back

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-hc1tk*_@gwk(b_fmq#rvsy#w6m*ahtj8it+48#&b!gh8r$ckpn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
